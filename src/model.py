51adec7f00f522f342a47329f2501f8cb6efed5a
Austin Zimmerman <austinzimmerman@Austins-Laptop.local>
2022-06-02 14:14:01 -0600

    syntax edits

--- src/model.py
+++ src/model.py
@@ -4,13 +4,14 @@ import markovify
 import nltk
 from nltk.corpus import gutenberg
 import warnings
-warnings.filterwarnings('ignore')nltk.download('gutenberg')
+warnings.filterwarnings('ignore')
+nltk.download('gutenberg')
 
 #inspect Gutenberg corpus
 print(gutenberg.fileids())
 
 #import novels as text objects
-hamlet = gutenberg.raw('shapespeare-hamlet.txt')
+hamlet = gutenberg.raw('shakespeare-hamlet.txt')
 macbeth = gutenberg.raw('shakespeare-macbeth.txt')
 caesar = gutenberg.raw('shakespeare-caesar.txt')#print first 100 characters of each
 print('\nRaw:\n', hamlet[:100])
@@ -43,7 +44,8 @@ caesar_doc = nlp(caesar)
 
 hamlet_sents = ' '.join([sent.text for sent in hamlet_doc.sents if len(sent.text) > 1])
 macbeth_sents = ' '.join([sent.text for sent in macbeth_doc.sents if len(sent.text) > 1])
-caesar_sents = ' '.join([sent.text for sent in caesar_doc.sents if len(sent.text) > 1])shakespeare_sents = hamlet_sents + macbeth_sents + caesar_sents
+caesar_sents = ' '.join([sent.text for sent in caesar_doc.sents if len(sent.text) > 1])
+shakespeare_sents = hamlet_sents + macbeth_sents + caesar_sents
 
 #inspect our text
 print(shakespeare_sents)
@@ -56,3 +58,19 @@ for i in range(3):
   print(generator_1.make_sentence())#We will randomly generate three more sentences of no more than 100 characters
 for i in range(3):
   print(generator_1.make_short_sentence(max_chars=100))
+
+#next we will use spacy's part of speech to generate more legible text
+class POSifiedText(markovify.Text):   
+    def word_split(self, sentence):
+      return ['::'.join((word.orth_, word.pos_)) for word in nlp(sentence)]   
+    def word_join(self, words):
+      sentence = ' '.join(word.split('::')[0] for word in words)
+      return sentence#Call the class on our text
+
+generator_2 = POSifiedText(shakespeare_sents, state_size=3)
+
+#now we will use the above generator to generate sentences
+for i in range(5):
+  print(generator_2.make_sentence())#print 100 characters or less sentences
+for i in range(5):
+  print(generator_2.make_short_sentence(max_chars=100))
